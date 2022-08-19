ab89b734727b72057fb11761ca7a5c54c4e7139c
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-08-19 13:51:57 +0200

    adding middlewares

--- 4-naturous/app.js
+++ 4-naturous/app.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware ✋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTE HANDLERS
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
@@ -98,6 +117,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3)  ROUTES
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -106,8 +127,9 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
-const port = 3000;
+// 4) START SERVER
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
