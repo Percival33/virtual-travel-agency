cb9ba4bde847960bb75b636e4142e8e531cbd134
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-10-08 13:20:48 +0200

    added fields filtering

--- 4-naturous/controllers/tourController.js
+++ 4-naturous/controllers/tourController.js
@@ -38,6 +38,14 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equals('easy');
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
