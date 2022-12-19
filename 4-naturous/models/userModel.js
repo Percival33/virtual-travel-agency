6c33fbe06717a2c4a8b191b71f957fa5baf079dd
Percival33 <marcin.jarc@gmail.com>
2022-12-19 20:18:09 +0100

    implement login endpoint

--- 4-naturous/models/userModel.js
+++ 4-naturous/models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: [8, 'A user password must have more or equal then 8 characters'],
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -44,6 +45,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = function (
+  candidatePassword,
+  userPassword
+) {
+  return bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
