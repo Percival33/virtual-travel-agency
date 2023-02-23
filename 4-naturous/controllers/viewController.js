860d79f3a6e2944c5363c9e7fd5fdd2d8fff7918
Percival33 <marcin.jarc@gmail.com>
2023-02-23 01:20:57 +0100

    created overview and tour views

--- 4-naturous/controllers/viewController.js
+++ 4-naturous/controllers/viewController.js
@@ -1,5 +1,5 @@
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -13,8 +13,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
   res.status(200).render('tour', {
     title: 'All tours',
+    tour,
   });
-};
+});
