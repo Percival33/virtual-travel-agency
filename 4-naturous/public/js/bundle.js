5fff2b35e201a036328eb35e6984f7b3361a4816
Percival33 <marcin.jarc@gmail.com>
2023-03-09 23:11:40 +0100

    implemented hacky stripe integration

--- 4-naturous/public/js/bundle.js
+++ 4-naturous/public/js/bundle.js
@@ -12458,7 +12458,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62124" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53389" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
