d540038ae52388749696ea6634722aa6d0625826
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-03-14 11:23:39 +0100

    fixed stripe error

--- 4-naturous/controllers/bookingController.js
+++ 4-naturous/controllers/bookingController.js
@@ -46,7 +46,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].unit_amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -66,10 +66,8 @@ exports.webhookCheckout = (req, res, next) => {
 
   if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
-
     res.status(200).json({ received: true });
   }
-  console.log(req.body);
 };
 
 exports.createBooking = factory.createOne(Booking);
